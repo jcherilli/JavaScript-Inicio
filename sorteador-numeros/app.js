3d15128c8d5d43e2b047c40ec3fd2da57983013d
JulioSantos <jcherilli@gmail.com>
2024-08-22 00:50:45 -0300

    botao reiniciar ativado

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -12,18 +12,42 @@ function sortear() {
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(numeroInicio, numeroFim);
         if (sorteados.includes(numero)) {
-console.log("numero ja sorteado - "+numero);
-i--;
+        console.log("numero ja sorteado - "+numero);
+        i--;
         } else {
             sorteados.push(numero);
         }
     }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
     
-    alert(sorteados);
+    // alert(sorteados);
+
+    alterarStatusBotao();
 }
 
 
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+            botao.classList.remove('container__botao-desabilitado');
+            botao.classList.add('container__botao');
+    } else {
+            botao.classList.remove('container__botao');
+            botao.classList.add('container__botao-desabilitado');
+    }
 }
