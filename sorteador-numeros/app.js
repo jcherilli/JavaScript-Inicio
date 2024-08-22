6ea93362e262debbcca64c3da5dff36346a9d28a
JulioSantos <jcherilli@gmail.com>
2024-08-22 00:27:20 -0300

    sorteando os numeros

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,8 +1,29 @@
 function sortear() {
-    let quantidade = document.getElementById('quantidade').value; 
-    let numeroInicio = document.getElementById('de').value; 
-    let numeroFim = document.getElementById('ate').value; 
-    alert(`Quantidade de números: ${quantidade}`); 
-    alert(`Do número: ${de}`); alert(`Até: ${ate}`); 
-    alert(`Do número: ${de}`); alert(`Até: ${ate}`); 
+    let quantidade = parseInt(document.getElementById('quantidade').value); 
+    let numeroInicio = parseInt(document.getElementById('de').value); 
+    let numeroFim = parseInt(document.getElementById('ate').value); 
+    // alert(`Quantidade de números: ${quantidade}`); 
+    // alert(`Do número: ${de}`); alert(`Até: ${ate}`); 
+    // alert(`Do número: ${de}`); alert(`Até: ${ate}`); 
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(numeroInicio, numeroFim);
+        if (sorteados.includes(numero)) {
+console.log("numero ja sorteado - "+numero);
+i--;
+        } else {
+            sorteados.push(numero);
+        }
+    }
+    
+    alert(sorteados);
+}
+
+
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
